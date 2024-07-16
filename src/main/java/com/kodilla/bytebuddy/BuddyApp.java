084ae27a42150c0ea0efc1277ccbd0bac2f37336
asmagala <asmagala@op.pl>
2024-07-16 15:35:05 +0200

    Init.

---
+++ src/main/java/com/kodilla/bytebuddy/BuddyApp.java
@@ -0,0 +1,23 @@
+package com.kodilla.bytebuddy;
+
+import net.bytebuddy.ByteBuddy;
+import net.bytebuddy.dynamic.loading.ClassLoadingStrategy.Default;
+import net.bytebuddy.implementation.FixedValue;
+import net.bytebuddy.matcher.ElementMatchers;
+
+public class BuddyApp {
+    public static void main(String[] args) throws Exception {
+        Class<?> dynamicBookClass = new ByteBuddy()
+                .subclass(Book.class)
+                .method(ElementMatchers.named("toString"))
+                .intercept(FixedValue.value("Hello my Buddy!"))
+                .make()
+                .load(Book.class.getClassLoader(), Default.WRAPPER)
+                .getLoaded();
+
+        Class[] parameterTypes = { String.class, String.class, int.class };
+
+        System.out.println(dynamicBookClass.getDeclaredConstructor(parameterTypes)
+                .newInstance("title", "author", 2010));
+    }
+}
